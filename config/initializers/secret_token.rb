6b062929317c0374a52268877d84c96ce4dfd717
Clay Stewart <claytonmstewart@gmail.com>
2013-12-07 10:36:04 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clownstagram::Application.config.secret_key_base = '1424728b5f7ceeb3974178a17567736ed28ae469fdf6659a6fc29a9f7119cd1f32df565f232e8d801b07b5babe8ad70a78a01609d60db92213e8378edca16aa5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clownstagram::Application.config.session_store :cookie_store, key: '_clownstagram_session'
